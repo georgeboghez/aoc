da6a7f5309fb1c3f6ae11a4cdd5f6b8d1680f0c2
georgeboghez <boghezgeorge@gmail.com>
2023-12-29 13:11:41 +0200

    start puzzle Day2 Part1

--- src/main/java/com/georgeboghez/aoc/days/day2/Day2.java
+++ src/main/java/com/georgeboghez/aoc/days/day2/Day2.java
@@ -2,6 +2,9 @@ package com.georgeboghez.aoc.days.day2;
 
 import com.georgeboghez.aoc.days.Day;
 import com.georgeboghez.aoc.dto.DaySolutionDto;
+import com.georgeboghez.aoc.enums.Part;
+import com.georgeboghez.aoc.enums.PuzzleStatusEnum;
+import com.georgeboghez.aoc.exceptions.PuzzleUnsolvedException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Day2 extends Day {
   @Override
   public DaySolutionDto solveFirstPart() {
-    return null;
+    throw new PuzzleUnsolvedException(getDayNumber(), Part.FIRST, PuzzleStatusEnum.IN_PROGRESS);
   }
 
   @Override
